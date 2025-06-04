35251b780692077774cf1a16121c71c7cf21f707
Gabriel Ribeiro <167247138+gabe-rbo@users.noreply.github.com>
2025-06-04 14:40:37 -0300

    Complete Meta-Interpreter

--- MIs/MI.pl
+++ MIs/MI.pl
@@ -2,11 +2,11 @@ mi(!, ChoicePoint) :- !, prolog_cut_to(ChoicePoint).
 mi(true, _) :- !.
 
 mi(Goal, _) :-
-    Goal \= (_, _), Goal\= (_; _), Goal \= (_, -> _), Goal \= !,
+    Goal \= (_, _), Goal\= (_; _), Goal \= (_ -> _), Goal \= !,
     predicate_property(Goal, built_in), !, call(Goal).
 
 mi(Goal, _) :-
-    Goal \= (_, _), Goal\= (_; _), Goal \= (_, -> _), Goal \= !,
+    Goal \= (_, _), Goal\= (_; _), Goal \= (_ -> _), Goal \= !,
     prolog_current_choice(ChoicePoint),
     clause(Goal, Body),
     mi(Body, ChoicePoint).
