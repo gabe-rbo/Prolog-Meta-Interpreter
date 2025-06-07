e18c3b921e600156c1c6dd78149555609602b478
Gabriel Ribeiro <167247138+gabe-rbo@users.noreply.github.com>
2025-06-07 19:24:38 -0300

    Compete Meta-interpreter

--- MIs/MI.pl
+++ MIs/MI.pl
@@ -1,7 +1,8 @@
 mi(Goal) :- prolog_current_choice(ChoicePoint), mi(Goal, ChoicePoint).
 
-mi(!, ChoicePoint) :- !, prolog_cut_to(ChoicePoint).
 mi(true, _) :- !.
+
+mi(!, ChoicePoint) :- !, prolog_cut_to(ChoicePoint).
 
 mi(Goal, _) :-
     Goal \= (_, _), Goal\= (_; _), Goal \= (_ -> _), Goal \= !,
@@ -19,6 +20,4 @@ mi((A, B), ChoicePoint) :-
 mi((A; B), ChoicePoint) :-
     mi(A, ChoicePoint); mi(B, ChoicePoint).
 
-mi((A -> B), ChoicePoint) :-
-    mi(A), !, mi(B, ChoicePoint).
-
+mi((A -> B), ChoicePoint) :- mi(A), !, mi(B, ChoicePoint).
