996900e6ea8de205e8d7d642ff646caa92ea39c8
Gabriel Ribeiro <167247138+gabe-rbo@users.noreply.github.com>
2025-06-15 21:34:35 -0300

    Complete Metainterpreter

---
+++ mi.pl
@@ -0,0 +1,25 @@
+mi(Goal) :- prolog_current_choice(ChoicePoint), mi(Goal, ChoicePoint).
+
+mi(true, _) :- !.
+
+mi(!, ChoicePoint) :- prolog_cut_to(ChoicePoint).
+
+mi(Goal, _) :-
+    Goal \= (_, _), Goal\= (_; _), Goal \= (_ -> _), Goal \= !,
+    predicate_property(Goal, built_in), !, call(Goal).
+
+mi(Goal, _) :-
+    Goal \= (_, _), Goal\= (_; _), Goal \= (_ -> _), Goal \= !,
+    prolog_current_choice(ChoicePoint), 
+    clause(Goal, Body),
+    mi(Body, ChoicePoint).
+
+mi((A, B), ChoicePoint) :- mi(A, ChoicePoint), mi(B, ChoicePoint).
+
+mi((A -> B), ChoicePoint) :- mi(A), !, mi(B, ChoicePoint).
+
+mi((A -> B; _), ChoicePoint) :- mi(A), !, mi(B, ChoicePoint).
+
+mi((_ -> _; C), ChoicePoint) :- !, mi(C, ChoicePoint).
+
+mi((A; B), ChoicePoint) :- mi(A, ChoicePoint); mi(B, ChoicePoint).
