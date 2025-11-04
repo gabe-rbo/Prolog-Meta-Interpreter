6a34ae56dc399495223a22cfddf469fa69f15fc7
gabe-rbo <gabrielribeiro11.contato@gmail.com>
2025-11-04 15:48:32 -0300

    Updates

--- ebg.pl
+++ ebg.pl
@@ -7,26 +7,12 @@ ebg(true, true, true, _ChoicePoint) :- !.
 ebg(!, !, true, ChoicePoint) :-
     prolog_cut_to(ChoicePoint).
 
-ebg(Goal, GenGoal, GenGoal, _ChoicePoint) :-
-    operational(GenGoal), !, call(Goal).
-
-ebg(Goal, GenGoal, GenGoal, _ChoicePoint) :-
-    not(operational(Goal)), Goal \= (_, _), Goal \= (_; _), Goal \= (_ -> _),
-    predicate_property(Goal, built_in), !, call(Goal).
-
-ebg(Goal, GenGoal, NewRule, _ChoicePoint) :-
-    not(operational(Goal)), not(predicate_property(Goal, built_in)),
-    prolog_current_choice(ChoicePoint),
-    clause(GenGoal, GenBody),
-    copy_term((GenGoal :- GenBody), (Goal :- Body)),
-    ebg(Body, GenBody, NewRule, ChoicePoint).
-
-ebg((A, B), (GenA, GenB), NewRule, ChoicePoint) :-
+ebg((A, B), (GenA, GenB), NewRule, ChoicePoint) :- !,
     ebg(A, GenA, NewRuleA, ChoicePoint),
     ebg(B, GenB, NewRuleB, ChoicePoint),
     simplify((NewRuleA, NewRuleB), NewRule).
 
-ebg((A -> B), (GenA -> GenB), NewRule, ChoicePoint) :-
+ebg((A -> B), (GenA -> GenB), NewRule, ChoicePoint) :- !,
     ebg(A, GenA, NewRuleA), !,
     ebg(B, GenB, NewRuleB, ChoicePoint),
     simplify((NewRuleA -> NewRuleB), NewRule).
@@ -39,10 +25,23 @@ ebg((A -> B; _), (GenA -> GenB), NewRule, ChoicePoint) :-
 ebg((_ -> _; C), GenC, NewRuleC, ChoicePoint) :- !,
     ebg(C, GenC, NewRuleC, ChoicePoint).
 
-ebg((A; B), (GenA; GenB), NewRule, ChoicePoint) :-
+ebg((A; B), (GenA; GenB), NewRule, ChoicePoint) :- !,
     (ebg(A, GenA, NewRuleA, ChoicePoint);
      ebg(B, GenB, NewRuleB, ChoicePoint)),
     simplify((NewRuleA; NewRuleB), NewRule).
+
+ebg(Goal, GenGoal, NewRule, _ChoicePoint) :-
+    not(operational(Goal)), not(predicate_property(Goal, built_in)),
+    prolog_current_choice(ChoicePoint),
+    clause(GenGoal, GenBody),
+    copy_term((GenGoal :- GenBody), (Goal :- Body)),
+    ebg(Body, GenBody, NewRule, ChoicePoint).
+	
+ebg(Goal, GenGoal, GenGoal, _ChoicePoint) :-
+	operational(GenGoal), !, call(Goal).
+
+ebg(Goal, GenGoal, GenGoal, _ChoicePoint) :-
+    predicate_property(Goal, built_in), call(Goal).
 
 simplify((true, NewRuleB), NewRuleB) :- !.
 simplify((NewRuleA, true), NewRuleA) :- !.
